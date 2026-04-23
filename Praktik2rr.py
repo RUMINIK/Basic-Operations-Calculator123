12d4bc12fcb3902ffbb73a8e0afaf7d528f66b49
RUMINIK <gr636_durda@lab-308-1.tomtit-tomsk.ru>
2026-04-23 13:41:14 +0700

    first commit

---
+++ Praktik2rr.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        return "Ошибка: деление на ноль"
+    return a / b
+
+if __name__ == "__main__":
+    print("Калькулятор:")
+    print(f"6 + 6 = {add(2, 3)}")
+    print(f"5 - 2 = {subtract(5, 2)}")
+    print(f"4 * 3 = {multiply(4, 3)}")
+    print(f"10 / 2 = {divide(10, 2)}")
